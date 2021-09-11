73429d2376315b55072cfe3a2ca473d396b75911
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-12 01:57:29 +0530

    Error resolve

--- src/main/java/com/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/employeepayroll/EmployeePayrollService.java
@@ -24,6 +24,8 @@ public class EmployeePayrollService {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService (employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
+
 
     }
 
@@ -41,4 +43,22 @@ public class EmployeePayrollService {
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 
     }
+
+    public void writeEmployeePayrollData(IOService ioService) {
+    	if (ioService.equals(IOService.CONSOLE_IO)) System.out.println("\nwriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+    	else if (ioService.equals(IOService.FILE_IO)) {
+    		new EmployeePayrollIOService().writeData(employeePayrollList);
+    	}
+    }
+
+	public void printData(IOService fileIo) {
+		if(fileIo.equals(IOService.FILE_IO)) new EmployeePayrollIOService().printData();
+	}
+
+
+	public long countEntries(IOService fileIo) {
+		if(fileIo.equals(IOService.FILE_IO)) return new EmployeePayrollIOService().countEntries();
+		
+		return 0;
+	}
 }
