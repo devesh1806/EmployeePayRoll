942e8bbee213f1153a6ad06027e08ec03c5a0031
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-12 01:59:34 +0530

    Error resolve

--- src/main/java/com/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/employeepayroll/EmployeePayrollService.java
@@ -61,4 +61,10 @@ public class EmployeePayrollService {
 		
 		return 0;
 	}
+
+	public long readData(IOService fileIo) {
+		if(fileIo.equals(IOService.FILE_IO)) return new EmployeePayrollIOService().readData().size();
+		return new ArrayList<>().size();
+	}
+	
 }
